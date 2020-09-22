b860ddfb1f11877a7ed0607b9ddc1671d04b591e
Viktor Gergely <viktor.gergely@erlang-solutions.com>
2020-09-22 11:15:28 +0200

    [add] dets backend structure, storage test

--- lib/ex_im/storage/dets.ex
+++ lib/ex_im/storage/dets.ex
@@ -5,26 +5,88 @@ defmodule ExIm.Storage.Dets do
     end)
   end
 
-  def write(table, key, value) do
-    :dets.insert(full_table_name(table), {key, value})
-  end
-
-  def read(table, key) do
-    :dets.lookup(full_table_name(table), key)
-  end
-
-  def delete(table, key) do
-    :dets.delete(full_table_name(table), key)
-  end
-
   def read_all() do
     Enum.map(Application.get_env(:ex_im, :tables, []), fn table ->
-      :dets.foldl(fn item, acc -> [item | acc] end, [], full_table_name(table))
+      :dets.foldl(
+        fn {key, value, version, deleted}, acc ->
+          [{table, key, {value, version, deleted}} | acc]
+        end,
+        [],
+        full_table_name(table)
+      )
     end)
     |> Enum.concat()
   end
 
+  def tables() do
+    Application.get_env(:ex_im, :tables, [])
+  end
+
+  def write(table, key, value) do
+    case raw_read(table, key) do
+      {:error, :not_found} ->
+        :dets.insert(full_table_name(table), {key, value, 1, false})
+
+      {old_value, version, true} ->
+        :dets.delete_object(full_table_name(table), {key, old_value, version, true})
+        :dets.insert(full_table_name(table), {key, value, version + 1, false})
+
+      {old_value, version, false} ->
+        :dets.delete_object(full_table_name(table), {key, old_value, version, false})
+        :dets.insert(full_table_name(table), {key, value, version + 1, false})
+    end
+  end
+
+  def read(table, key) do
+    case raw_read(table, key) do
+      {value, _, false} ->
+        {:ok, value}
+
+      _ ->
+        {:error, :not_found}
+    end
+  end
+
+  def delete(table, key) do
+    case raw_read(table, key) do
+      {value, version, false} ->
+        :dets.delete(full_table_name(table), key)
+        :dets.insert(full_table_name(table), {key, value, version, true})
+        :ok
+
+      _ ->
+        :ok
+    end
+  end
+
   defp full_table_name(table) do
     to_charlist(table) ++ '_' ++ to_charlist(node())
+  end
+
+  defp find_latest([{_, value, version, deleted} | values]) do
+    find_latest(values, {value, version, deleted})
+  end
+
+  defp find_latest([{_, new_value, new_version, new_deleted} | values], {_, old_version, _})
+       when new_version > old_version do
+    find_latest(values, {new_value, new_version, new_deleted})
+  end
+
+  defp find_latest([_ | values], value) do
+    find_latest(values, value)
+  end
+
+  defp find_latest([], value) do
+    value
+  end
+
+  defp raw_read(table, key) do
+    case :dets.lookup(full_table_name(table), key) do
+      [] ->
+        {:error, :not_found}
+
+      values when is_list(values) ->
+        find_latest(values)
+    end
   end
 end
--- test/ex_im/storage_test.exs
+++ test/ex_im/storage_test.exs
@@ -1,3 +1,32 @@
 defmodule ExIm.StorageTest do
   use ExUnit.Case
+
+  alias ExIm.Storage.{Dets, Process}
+
+  setup_all do
+    Application.ensure_all_started(:ex_im)
+  end
+
+  test "process storage" do
+    run_test(Process)
+  end
+
+  test "dets storage" do
+    run_test(Dets)
+  end
+
+  defp run_test(module) do
+    module.init_tables([:test_table])
+    module.delete(:test_table, :a)
+    module.write(:test_table, :b, :c)
+    assert [{:test_table, :b, {:c, _, false}}] = module.read_all()
+    assert {:ok, :c} = module.read(:test_table, :b)
+    assert {:error, :not_found} = module.read(:test_table, :c)
+    module.delete(:test_table, :b)
+    assert [{:test_table, :b, {:c, _, true}}] = module.read_all()
+    module.write(:test_table, :b, :c)
+    assert [{:test_table, :b, {:c, _, false}}] = module.read_all()
+    module.write(:test_table, :b, :d)
+    assert [{:test_table, :b, {:d, _, false}}] = module.read_all()
+  end
 end
